458b02c07f8e69f6bd1687fcf50046b52bf35d6f
JojoDev-pro <joelRJ2525@gmail.com>
2024-09-29 20:57:52 +0000

    modifica a frase

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return <h1>Eu vou ser o maior programador de todos</h1>
+    return <h1>Eu vou ser o maior programador de todos, podem acreditar</h1>
 }
 
 export default Home;
