5504fc17e3b75c2ff0618076fde514116a6120ba
JojoDev-pro <joelRJ2525@gmail.com>
2024-10-08 20:21:41 +0000

    add `database.js`and `.env`files

--- pages/index.js
+++ pages/index.js
@@ -2,8 +2,4 @@ function Home() {
   return <h1>Uno vampiresco, socorro !!!!</h1>;
 }
 
-function teste() {
-  console.log("teste");
-}
-
 export default Home;
--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -1,4 +1,4 @@
-test("get TO /API/V1/STATUS SHOULD RETURN 200", async () => {
+test("GET to /api/v1/status should return 200", async () => {
   const response = await fetch("http://localhost:3000/api/v1/status");
   expect(response.status).toBe(200);
 });
